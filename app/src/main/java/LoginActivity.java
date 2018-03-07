81e39038cf462703fa12b9849ee168bcbfbf8164
chiendo.2412 <chiendo.15t4@gmail.com>
2018-03-07 16:42:54 +0700

    commit changed

---
+++ app/src/main/java/LoginActivity.java
@@ -0,0 +1,9 @@
+import android.app.Application;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by SILVER on 3/7/2018.
+ */
+
+public class LoginActivity  extends AppCompatActivity{
+}
